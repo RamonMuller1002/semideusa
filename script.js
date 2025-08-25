1876a06daa7b1d5bb422ef0c338b689308709552
RamonMuller1002 <ramonmuller1002@gmail.com>
2025-08-25 10:06:50 -0300

    Update rotoas imgs e Upload de capas de albuns

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // Data alvo: 13 de abril de 2025 (formato: ano, mês (0-11), dia)
 const targetDate = new Date(2025, 3, 14); // mês 3 = abril
 function updateCounter() {
-    hora = Date.now();
-    const now = new Date(hora - (3600000 * 3));
+    const hora = Date.now();
+    const now = new Date();
     const diff = now - targetDate;
     const totalMinutes = Math.floor(diff / (1000 * 60));
-    const days = Math.floor(totalMinutes / (60 * 24));
-    const hours = Math.floor((totalMinutes % (60 * 24)) / 60);
-    const minutes = totalMinutes % 60;
-    const seconds = Math.floor((diff / 1000) % 60);
+    let days = Math.floor(totalMinutes / (60 * 24));
+    let hours = Math.floor((totalMinutes % (60 * 24)) / 60);
+    let minutes = totalMinutes % 60;
+    let seconds = Math.floor((diff / 1000) % 60);
     
     document.getElementById('counter').innerHTML = `${days} dias, ${hours} horas, ${minutes < 10 ? '0' : ''}${minutes} minutos e ${seconds < 10 ? '0' : ''}${seconds} segundos de namoro!`;
 }
